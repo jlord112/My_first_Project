f8a8da67d75e5eadb074612314a3cd4f572a4bb1
Juan Ordoñez <jj1015ny@yahoo.com>
2015-08-11 13:08:03 -0400

    FirstProject
    Learning python

---
+++ First_Project.py
@@ -0,0 +1,9 @@
+
+firstName = input (" whats your name ? ")
+lastName = input (" whats your last name ? ")
+country = input ("What country are you from ? ")
+country = country.upper()
+print(country.count("d"))
+city = input ("whats city are you from ? ")
+city = cityvv v .capitalize()
+print("Hello" + "    " + firstName + " " + lastName + " "+ "From" +" " + city + " " + country )
